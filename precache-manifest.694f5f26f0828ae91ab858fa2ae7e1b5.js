9506a91003cbea24facade6861d0101f41823133
Leonardo <leonardo-assuncao1@hotmail.com>
2019-07-17 17:17:35 -0300

    Primeiro commit

---
+++ precache-manifest.694f5f26f0828ae91ab858fa2ae7e1b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec36f98ba1f72691afda62d3f4bf6edd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f653e750d771d2434cfc",
+    "url": "/static/css/main.a6f0e216.chunk.css"
+  },
+  {
+    "revision": "8f15a960ee2ebb1cf6ae",
+    "url": "/static/js/2.093c65eb.chunk.js"
+  },
+  {
+    "revision": "f653e750d771d2434cfc",
+    "url": "/static/js/main.d277a3cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
